7132c862995037733fc15190eee2084f1e519a24
Tran Thien Trong <tttrong@vlab.tech>
2020-10-12 10:45:12 +0700

    init

--- HCM/src/main/java/vlab/tech/hcm/MainActivity.java
+++ HCM/src/main/java/vlab/tech/hcm/MainActivity.java
@@ -4,8 +4,7 @@ import android.location.Location;
 
 public class MainActivity {
     public static float getDistance(double a, double b, double c, double d){
-        float[] result = new float[0];
-        Location.distanceBetween(a,b,c,d,result);
-        return result[0]/1000;
+
+        return 0;
     }
 }
--- app/src/main/java/vlab/tech/landinfo/MainActivity.java
+++ app/src/main/java/vlab/tech/landinfo/MainActivity.java
@@ -3,6 +3,9 @@ package vlab.tech.landinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+        float distance = vlab.tech.hcm.MainActivity.getDistance(0,0,0,0);
+        tv.setText("heheh");
+        Log.d("hehe", String.valueOf(distance));
     }
 }
