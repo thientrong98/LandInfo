6a349e93ee3fd7858242e10b7ad00a05f83d2c15
Tran Thien Trong <tttrong@vlab.tech>
2020-10-12 10:55:37 +0700

    fix bug

--- HCM/src/main/java/vlab/tech/hcm/MainActivity.java
+++ HCM/src/main/java/vlab/tech/hcm/MainActivity.java
@@ -3,7 +3,7 @@ package vlab.tech.hcm;
 import android.location.Location;
 
 public class MainActivity {
-    public static String getDistance(double a, double b, double c, double d){
-        return "hehe";
+    public static int Plus(int a, int b){
+        return a+b;
     }
 }
